e0a0911218615764bab6801785769b01d8343a99
Executive_Eagle <christophershelley257@gmail.com>
2026-08-14 21:57:43 -0500

    Add isolated account provisioning auth

--- js/firebase.js
+++ js/firebase.js
@@ -1,8 +1,28 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.17.1/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/12.17.1/firebase-auth.js";
+import {
+  browserLocalPersistence,
+  getAuth,
+  inMemoryPersistence,
+  setPersistence
+} from "https://www.gstatic.com/firebasejs/12.17.1/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.17.1/firebase-firestore.js";
 import { firebaseConfig } from "./firebase-config.js";
 
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+
+// Founder-created Auth users are provisioned through an isolated secondary app.
+// Creating a user signs that Auth instance in as the new user, so using a separate
+// in-memory instance prevents the Founder Director's primary portal session from
+// being replaced during account creation.
+export const provisioningApp = initializeApp(firebaseConfig, "accountProvisioner");
+export const provisioningAuth = getAuth(provisioningApp);
+
+setPersistence(auth, browserLocalPersistence).catch((error) => {
+  console.error("Unable to configure primary authentication persistence", error);
+});
+
+setPersistence(provisioningAuth, inMemoryPersistence).catch((error) => {
+  console.error("Unable to configure provisioning authentication persistence", error);
+});
