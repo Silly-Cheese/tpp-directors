9b83a01f36ba199405a301f74b105ae246595732
Executive_Eagle <christophershelley257@gmail.com>
2026-08-14 21:47:55 -0500

    Add Firebase web configuration

---
+++ js/firebase-config.js
@@ -0,0 +1,16 @@
+export const firebaseConfig = Object.freeze({
+  apiKey: "AIzaSyCNnvsowo030LSM3OkCKcKfWAcw0xu-kOw",
+  authDomain: "tpp-direc.firebaseapp.com",
+  projectId: "tpp-direc",
+  storageBucket: "tpp-direc.firebasestorage.app",
+  messagingSenderId: "376518680917",
+  appId: "1:376518680917:web:b2529aece8a0ee8d024fdc"
+});
+
+export const portalConfig = Object.freeze({
+  organizationName: "The Prayer Project",
+  portalName: "Board of Directors Portal",
+  productionHost: "directors.ask4prayers.com",
+  firebaseServices: ["Authentication", "Cloud Firestore"],
+  documentPolicy: "google-links-only"
+});
