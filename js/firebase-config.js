1ac8fe96eedd9f7347680c0b2f1187ecb482f757
Executive_Eagle <christophershelley257@gmail.com>
2026-08-14 21:53:02 -0500

    Limit client configuration to Auth and Firestore needs

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -2,8 +2,6 @@ export const firebaseConfig = Object.freeze({
   apiKey: "AIzaSyCNnvsowo030LSM3OkCKcKfWAcw0xu-kOw",
   authDomain: "tpp-direc.firebaseapp.com",
   projectId: "tpp-direc",
-  storageBucket: "tpp-direc.firebasestorage.app",
-  messagingSenderId: "376518680917",
   appId: "1:376518680917:web:b2529aece8a0ee8d024fdc"
 });
 
